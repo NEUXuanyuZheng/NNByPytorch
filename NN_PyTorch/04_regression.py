9fe2e2ba9c93bf63ec1cf5030a0d4bc87a57a2fa
zhengxuanyu <40654928+zhengxuanyu@users.noreply.github.com>
2019-03-28 22:19:49 +0800

    Update 04_regression.py

--- NN_PyTorch/04_regression.py
+++ NN_PyTorch/04_regression.py
@@ -1,54 +1,43 @@
 import torch
-from torch.autograd import Variable
+import torch.nn as nn
 import torch.nn.functional as F
 import matplotlib.pyplot as plt
 
 
-x = torch.unsqueeze(torch.linspace(-1, 1, 100), dim=1) #dim 1 to dim 2
-y = x.pow(2) + 0.2*torch.rand(x.size())
+x = torch.unsqueeze(torch.linspace(-1, 1, 100), dim=1)
+y = x.pow(2) + 0.2 * torch.rand(x.size())
 
-x, y = Variable(x), Variable(y)
-
-# plt.scatter(x.data.numpy(), y.data.numpy())
-# plt.show()
-
-
-class Net(torch.nn.Module):
-    # define layers of neural network
-    def __init__(self, n_feature, n_hidden_neuron, n_output):
+class Net(nn.Module):
+    def __init__(self, n_features, n_hidden_nuron, n_outputs):
         super(Net, self).__init__()
-        self.hidden = torch.nn.Linear(n_feature, n_hidden_neuron)
-        self.predict = torch.nn.Linear(n_hidden_neuron, n_output)
+        self.hidden = nn.Linear(n_features, n_hidden_nuron)
+        self.predict = nn.Linear(n_hidden_nuron, n_outputs)
 
-    # build a neural network
     def forward(self, x):
-        x = torch.relu(self.hidden(x))
+        x = F.relu(self.hidden(x))
         x = self.predict(x)
         return x
 
 
 net = Net(1, 10, 1)
-print(net)
 
 plt.ion()
 
 optimizer = torch.optim.SGD(net.parameters(), lr=0.5)
-loss_func = torch.nn.MSELoss()
+loss_func = nn.MSELoss()
 
 for t in range(100):
     prediction = net(x)
-
     loss = loss_func(prediction, y)
-
-    optimizer.zero_grad() # set all gradient 0
+    optimizer.zero_grad()
     loss.backward()
     optimizer.step()
+
     if t % 5 == 0:
         plt.cla()
         plt.scatter(x.data.numpy(), y.data.numpy())
         plt.plot(x.data.numpy(), prediction.data.numpy(), 'r-', lw=5)
         plt.text(0.5, 0, 'Loss=%.4f' % loss.data.numpy(), fontdict={'size': 20, 'color': 'red'})
-        plt.pause(0.1)
+        plt.show()
 
 plt.ioff()
-plt.show()
